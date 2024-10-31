a129af33a20cf21f7ab7cf2cbdb7c4dde6ac8e3b
Harikrishnan P <harikrishnan.p@experionglobal.local>
2024-10-31 10:12:00 +0530

    authentication updates on report page

--- scripts/report-page.js
+++ scripts/report-page.js
@@ -1,3 +1,13 @@
+import { checkAuth, logout } from './auth.js';
+
+// Check authentication status
+checkAuth();
+
+// Attach logout function to the logout button
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.logout').addEventListener('click', logout);
+});
+
 const images = document.querySelectorAll('.image-container img');
 const templates = document.querySelectorAll('.workspace > div');
 
